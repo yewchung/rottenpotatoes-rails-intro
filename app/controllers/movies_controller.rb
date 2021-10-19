2f8029831764a6cce604ec6817e746d24a6c0bbf
Philip Green <yewchung@berkeley.edu>
2021-10-19 23:44:38 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,13 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    if (!params[:ratings].nil?)
+      @ratings_to_show = params[:ratings].keys
+    else
+      @ratings_to_show = @all_ratings
+    end
+    @movies = Movie.with_ratings(@ratings_to_show)
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,19 @@
 class Movie < ActiveRecord::Base
+  
+   def self.all_ratings
+     ratings = []
+     Movie.select(:rating).distinct.each do |r|
+       ratings.push(r.rating)
+     end
+     return ratings
+   end
+  
+   def self.with_ratings(ratings_list)
+     if (ratings_list.nil?)
+       return Movie.all()
+     else
+       return Movie.where(rating: ratings_list)
+     end
+   end
+     
 end
